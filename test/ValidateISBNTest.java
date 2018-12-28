2b5648f7cd3474dedd9cca7440b5141f10d1b781
Bartosz <bg@bartoszgajda.com>
2018-12-28 19:29:38 +0000

    First failing test.

--- test/ValidateISBNTest.java
+++ test/ValidateISBNTest.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 class ValidateISBNTest {
 
+  @Test
+  public void test() {
+//    fail("not yet implemented");
+  }
 }
