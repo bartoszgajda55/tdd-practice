98660c6a45f60c99e5b38fc8dfa370abee726cf6
Bartosz <bg@bartoszgajda.com>
2018-12-30 20:16:45 +0000

    Writing first test and method.

---
+++ src/com/bartoszgajda/ValidateISBN.java
@@ -0,0 +1,8 @@
+package com.bartoszgajda;
+
+public class ValidateISBN {
+
+  public boolean checkISBN(long l) {
+    return false;
+  }
+}
--- test/ValidateISBNTest.java
+++ test/ValidateISBNTest.java
@@ -1,11 +1,14 @@
+import com.bartoszgajda.ValidateISBN;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ValidateISBNTest {
 
   @Test
-  public void test() {
-//    fail("not yet implemented");
+  public void checkValidISBN() {
+    ValidateISBN validator = new ValidateISBN();
+    boolean result = validator.checkISBN(140449116L);
+    assertTrue(result);
   }
 }
