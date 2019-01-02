b55602c6f041a45eac61b624a8ad2170e6198484
Bartosz <bg@bartoszgajda.com>
2019-01-02 21:15:41 +0000

    Writing some code for testing.

--- test/ValidateISBNTest.java
+++ test/ValidateISBNTest.java
@@ -9,16 +9,14 @@ class ValidateISBNTest {
   @Test
   public void checkValidISBN() {
     ValidateISBN validator = new ValidateISBN();
-    boolean result = validator.checkISBN(140449116);
-    assertTrue(result, "first value");
-    result = validator.checkISBN(140177396);
-    assertTrue(result, "second value");
+    boolean result = validator.checkISBN("0140449116");
+    assertTrue(result);
   }
 
   @Test
   public void checkInvalidISBN() {
     ValidateISBN validator = new ValidateISBN();
-    boolean result = validator.checkISBN(140449117);
+    boolean result = validator.checkISBN("0140449117");
     assertFalse(result);
   }
 }
