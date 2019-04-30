8a6d3496a0e12d5f1decb4a3364cc23a09043b15
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-30 21:24:42 +0100

    Add Test file for BankAccount class.

---
+++ test/main/java/com/bartoszgajda/BankAccountTest.java
@@ -0,0 +1,19 @@
+package main.java.com.bartoszgajda;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+  @Test
+  public void deposit() {
+  }
+
+  @Test
+  public void withdraw() {
+  }
+
+  @Test
+  public void getBalance() {
+  }
+}
