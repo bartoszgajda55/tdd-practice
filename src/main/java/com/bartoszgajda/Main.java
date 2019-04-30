36f02a7dc6f0bf23edb97d62fac4518a23e0a49f
Bartosz Gajda <bg@bartoszgajda.com>
2019-04-30 22:41:41 +0100

    Add test with assertion.

--- src/main/java/com/bartoszgajda/Main.java
+++ src/main/java/com/bartoszgajda/Main.java
@@ -2,5 +2,7 @@ package main.java.com.bartoszgajda;
 
 public class Main {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    System.out.println("The app is running correctly");
+  }
 }
--- test/main/java/com/bartoszgajda/BankAccountTest.java
+++ test/main/java/com/bartoszgajda/BankAccountTest.java
@@ -7,13 +7,21 @@ public class BankAccountTest {
 
   @Test
   public void deposit() {
+    fail("This test is yet to be implemented");
   }
 
   @Test
   public void withdraw() {
+    fail("This test is yet to be implemented");
   }
 
   @Test
   public void getBalance() {
+    fail("This test is yet to be implemented");
+  }
+
+  @Test
+  public void dummyTest() {
+    assertEquals(20, 21);
   }
 }
