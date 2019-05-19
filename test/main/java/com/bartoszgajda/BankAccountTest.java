8e68de61cae89a020a532d1b2f1092c0cc990dad
Bartosz Gajda <bg@bartoszgajda.com>
2019-05-19 17:02:04 +0200

    Add setup to test class.

--- test/main/java/com/bartoszgajda/BankAccountTest.java
+++ test/main/java/com/bartoszgajda/BankAccountTest.java
@@ -1,13 +1,19 @@
 package main.java.com.bartoszgajda;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
+  private BankAccount account;
+
+  @Before
+  public void setup() {
+    account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
+  }
 
   @Test
   public void deposit() {
-    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
     double balance = account.deposit(200.00, true);
     assertEquals(1200.00, balance, 0);
   }
@@ -19,27 +25,18 @@ public class BankAccountTest {
 
   @Test
   public void getBalance_deposit() {
-    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
     account.deposit(200.00, true);
     assertEquals(1200.00, account.getBalance(), 0);
   }
 
   @Test
   public void getBalance_withdraw() {
-    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
     account.withdraw(200.00, true);
     assertEquals(800.00, account.getBalance(), 0);
   }
 
   @Test
   public void isChecking__true() {
-    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
     assertTrue(account.isChecking());
-  }
-
-  @Test
-  public void isChecking__false() {
-    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.SAVINGS);
-    assertFalse(account.isChecking());
   }
 }
