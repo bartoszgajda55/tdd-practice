41aaf85d8feb17f8918b97ed85fc4917c9db2abc
Bartosz Gajda <bg@bartoszgajda.com>
2019-05-19 16:59:17 +0200

    Update test with assertTrue and assertFalse.

--- src/main/java/com/bartoszgajda/BankAccount.java
+++ src/main/java/com/bartoszgajda/BankAccount.java
@@ -5,10 +5,16 @@ public class BankAccount {
   private String lastName;
   private double balance;
 
-  public BankAccount(String firsName, String lastName, double balance) {
+  public static final int CHECKING = 1;
+  public static final int SAVINGS = 2;
+
+  private int accountType;
+
+  public BankAccount(String firsName, String lastName, double balance, int accountType) {
     this.firsName = firsName;
     this.lastName = lastName;
     this.balance = balance;
+    this.accountType = accountType;
   }
 
   /**
@@ -41,5 +47,9 @@ public class BankAccount {
 
   public double getBalance() {
     return balance;
+  }
+
+  public boolean isChecking() {
+    return accountType == CHECKING;
   }
 }
--- test/main/java/com/bartoszgajda/BankAccountTest.java
+++ test/main/java/com/bartoszgajda/BankAccountTest.java
@@ -7,7 +7,7 @@ public class BankAccountTest {
 
   @Test
   public void deposit() {
-    BankAccount account = new BankAccount("John", "Doe", 1000.00);
+    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
     double balance = account.deposit(200.00, true);
     assertEquals(1200.00, balance, 0);
   }
@@ -18,16 +18,28 @@ public class BankAccountTest {
   }
 
   @Test
-  public void getBalance__deposit() {
-    BankAccount account = new BankAccount("John", "Doe", 1000.00);
+  public void getBalance_deposit() {
+    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
     account.deposit(200.00, true);
     assertEquals(1200.00, account.getBalance(), 0);
   }
 
   @Test
-  public void getBalance__withdraw() {
-    BankAccount account = new BankAccount("John", "Doe", 1000.00);
+  public void getBalance_withdraw() {
+    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
     account.withdraw(200.00, true);
     assertEquals(800.00, account.getBalance(), 0);
+  }
+
+  @Test
+  public void isChecking__true() {
+    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.CHECKING);
+    assertTrue(account.isChecking());
+  }
+
+  @Test
+  public void isChecking__false() {
+    BankAccount account = new BankAccount("John", "Doe", 1000.00, BankAccount.SAVINGS);
+    assertFalse(account.isChecking());
   }
 }
