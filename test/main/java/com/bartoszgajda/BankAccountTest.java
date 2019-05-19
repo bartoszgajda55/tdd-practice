90155632a4e8cf5508f10c4cb2aadf0fb5a2ca77
Bartosz Gajda <bg@bartoszgajda.com>
2019-05-19 16:53:20 +0200

    Add some real tests.

--- test/main/java/com/bartoszgajda/BankAccountTest.java
+++ test/main/java/com/bartoszgajda/BankAccountTest.java
@@ -7,7 +7,9 @@ public class BankAccountTest {
 
   @Test
   public void deposit() {
-    fail("This test is yet to be implemented");
+    BankAccount account = new BankAccount("John", "Doe", 1000.00);
+    double balance = account.deposit(200.00, true);
+    assertEquals(1200.00, balance, 0);
   }
 
   @Test
@@ -16,12 +18,16 @@ public class BankAccountTest {
   }
 
   @Test
-  public void getBalance() {
-    fail("This test is yet to be implemented");
+  public void getBalance__deposit() {
+    BankAccount account = new BankAccount("John", "Doe", 1000.00);
+    account.deposit(200.00, true);
+    assertEquals(1200.00, account.getBalance(), 0);
   }
 
   @Test
-  public void dummyTest() {
-    assertEquals(20, 21);
+  public void getBalance__withdraw() {
+    BankAccount account = new BankAccount("John", "Doe", 1000.00);
+    account.withdraw(200.00, true);
+    assertEquals(800.00, account.getBalance(), 0);
   }
 }
